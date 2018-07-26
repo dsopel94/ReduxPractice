6d67bf207ed821929987b6e9722b3c8515d2b1a6
dsopel94 <dsopel0@gmail.com>
2018-07-25 21:53:39 -0500

    Added more actions and reducers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
+    {title: 'JavaScript: The Good Parts', pages: 81},
+    {title: 'Harry Potter', pages: 83},
+    {title: 'The Dark Tower', pages: 200},
+    {title: 'Eloquent Ruby', pages: 50}
   ]
 }
